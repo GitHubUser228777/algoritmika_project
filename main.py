0f3dd759da7250b5503aaa9191525fb7c8c2daf3
GitHubUser228777 <98205655+GitHubUser228777@users.noreply.github.com>
2023-06-18 16:13:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
